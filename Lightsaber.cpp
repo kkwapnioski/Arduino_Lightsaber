0ba85d5a20a3ff5a374436e6ceae9bc033e05aed
Kyle <kfkwapni@up.com>
2015-12-09 17:01:31 -0600

    Updated

--- Lightsaber.cpp
+++ Lightsaber.cpp
@@ -2,10 +2,24 @@
 #include <SoftwareSerial.h>
 #include <DFPlayer.h>
 
+
 Lightsaber::Lightsaber() {
-  state_ = INITIALIZING;
+  state_ = LSS_Initializing;
 }
+
 
 Lightsaber::~Lightsaber() {
+  delete dfplayer;
 }
 
+
+void Lightsaber::initialize_sound(SerialMode mode, int rxPin, int txPin) {
+  // initialize dfplayer sound module
+  dfplayer = new DFPlayer(mode, rxPin, txPin);
+  
+}
+
+
+void Lightsaber::loop() {
+  
+}
--- Lightsaber.h
+++ Lightsaber.h
@@ -2,30 +2,71 @@
 #define Lightsaber_h
 
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 #include <DFPlayer.h>
 
+
+#define LS_SOUND_DIR_SYSTEM = 1
+#define LS_SOUND_DIR_POWERON = 2
+#define LS_SOUND_DIR_IDLE = 3
+#define LS_SOUND_DIR_SWING = 4
+#define LS_SOUND_DIR_CLASH = 5
+#define LS_SOUND_DIR_LOCKUP = 7
+#define LS_SOUND_DIR_DEFLECT = 8
+#define LS_SOUND_DIR_POWEROFF = 9
+
+
+
 enum LightsaberState {
-  INITIALIZING,
-  OFF,
-  POWERING_ON,
-  IDLE,
-  SWINGING,
-  CLASHING,
-  LOCKUP,
-  DEFLECT,
-  POWERING_OFF
+  LSS_Initializing,
+  LSS_Off,
+  LSS_PoweringOn,
+  LSS_Idle,
+  LSS_Swinging,
+  LSS_Clashing,
+  LSS_Lockup,
+  LSS_Deflect,
+  LSS_PoweringOff
 };
 
+
 class Lightsaber {
+  
   public:
     Lightsaber();
     ~Lightsaber();
+    void initialize_power_button(int pin);
+    void initialize_sound(SerialMode mode, int rxPin, int txPin);
+    void loop();
+    
+    void power_on();
+    void power_on(int poweron);
+    void power_off();
+    void power_off(int poweroff);
+    void swing();
+    void swing(int swing);
+    void clash();
+    void clash(int clash);
+    
   
   private:
+    // static members
+    static const int SOUND_LOCKOUT      = 500;
+    static const int SOUND_IDLE_TIMEOUT = 1500;
+    
+    // members
     LightsaberState state_;
     DFPlayer* dfplayer;
-  
+    int numPowerOn_;
+    int numIdle_;
+    int numSwing_;
+    int numClash_;
+    int numLockup_;
+    int numDeflect_;
+    int numPowerOff_;
+    
+    
+    // methods
+    
 };
 
 #endif
